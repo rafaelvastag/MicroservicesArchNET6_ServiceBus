e7deb0d48feabd206110a080f7dffe07ce252254
Rafael Vastag <rgvastag@gmail.com>
2021-12-16 12:16:27 -0300

    first commit

---
+++ ProductAPI/DbContext/ApplicationContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductAPI.Models;
+
+namespace ProductAPI
+{
+    public class ApplicationContext : DbContext
+    {
+
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<ProductEntity> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
