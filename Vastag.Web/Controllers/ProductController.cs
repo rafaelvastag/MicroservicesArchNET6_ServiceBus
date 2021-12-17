a62b519ac5750ba9760778f97908a6acb3fe8d1e
Rafael Vastag <rgvastag@gmail.com>
2021-12-16 21:52:40 -0300

    edit products view

---
+++ Vastag.Web/Controllers/ProductController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using ProductAPI.Models.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Vastag.Web.Services;
+
+namespace Vastag.Web.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IProductService _productService;
+
+        public ProductController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        public async Task<IActionResult> ProductIndex()
+        {
+
+            List<ProductDTO> list = new();
+            var response = await _productService.GetAllProductsAsync<ResponseDTO>();
+
+            if (null != response && response.IsSuccess)
+            {
+                list = JsonConvert.DeserializeObject<List<ProductDTO>>(Convert.ToString(response.Result));
+            }
+
+            return View(list);
+        }
+    }
+}
