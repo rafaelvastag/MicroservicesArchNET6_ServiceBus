159f34fb56d42df2941cb5e33277f4f419c187e7
Rafael Vastag <rgvastag@gmail.com>
2021-12-20 11:33:25 -0300

    add home details

--- Vastag.Web/Controllers/HomeController.cs
+++ Vastag.Web/Controllers/HomeController.cs
@@ -2,24 +2,54 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.Threading.Tasks;
 using Vastag.Web.Models;
+using Vastag.Web.Models.DTOs;
+using Vastag.Web.Services;
 
 namespace Vastag.Web.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IProductService _productService;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IProductService productService)
         {
             _logger = logger;
+            _productService = productService;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            List<ProductDTO> products = new List<ProductDTO>();
+            var accessToken = await HttpContext.GetTokenAsync("access_token");
+            var response = await _productService.GetAllProductsAsync<ResponseDTO>(accessToken);
+
+            if (response != null && response.IsSuccess)
+            {
+                products = JsonConvert.DeserializeObject<List<ProductDTO>>(Convert.ToString(response.Result));
+            }
+            
+            return View(products);
+        }
+
+        public async Task<IActionResult> Details(int id)
+        {
+            ProductDTO product = new();
+            var accessToken = await HttpContext.GetTokenAsync("access_token");
+            var response = await _productService.GetProductByIdAsync<ResponseDTO>(id,accessToken);
+
+            if (response != null && response.IsSuccess)
+            {
+                product = JsonConvert.DeserializeObject<ProductDTO>(Convert.ToString(response.Result));
+            }
+
+            return View(product);
         }
 
         public IActionResult Privacy()
--- Vastag.Web/Models/DTOs/ProductDTO.cs
+++ Vastag.Web/Models/DTOs/ProductDTO.cs
@@ -1,12 +1,29 @@
-﻿namespace Vastag.Web.Models.DTOs
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Vastag.Web.Models.DTOs
 {
     public class ProductDTO
     {
+        public ProductDTO()
+        {
+            Count = 1;
+        }
+
         public int Id { get; set; }
         public string Name { get; set; }
         public double Price { get; set; }
         public string Description { get; set; }
         public string CategoryName { get; set; }
         public string ImageUrl { get; set; }
+
+        [Range(1,100)]
+        public int Count { get; set; }
+
+        public static implicit operator List<object>(ProductDTO v)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
